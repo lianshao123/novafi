a8ebb301faac602ef2b11e963a5307046a89004e
peng <1063629816@qq.com>
2025-01-23 14:48:24 +0800

    add block U

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,12 +9,12 @@ import { abi } from "@/config/EchoooMallPayment.json";
 import { abi as erc20Abi } from "@/config/Erc20.json";
 
 type FieldType = {
-  transactionId?: string; // 交易 id
-  amount?: string; // 交易数量
-  merchanAddress?: string; // 商户地址
-  receivingAddress?: string; // 接收地址
-  currency?: string; // 选择的币种
   transactionIdBytes32?: string;
+  receivingAddress?: string;
+  currency?: string;
+  amount?: string;
+  merchanAddress?: string;
+  address?: string;
 };
 
 const { Option } = Select;
@@ -26,52 +26,72 @@ export default function Index() {
   const [form] = Form.useForm();
   const { data: walletClient } = useWalletClient();
   const config = useConfig();
-  const [isWithdraw, setIsWithdraw] = useState(false); // 状态：是否为“提取资金”
-  const [isRefund, setIsRefund] = useState(false); // 状态：是否为“退款”
+  const [currentAction, setCurrentAction] = useState("order"); // 当前操作类型
   const [txHash, setTxHash] = useState<string>("");
   const [txIdBytes32, setTxIdBytes32] = useState<string>("");
 
   const onFinish: FormProps<FieldType>["onFinish"] = async (values) => {
     try {
-      // 下单逻辑
       if (!walletClient) {
         console.error("No wallet client found.");
         return;
       }
-      if (isWithdraw) {
-        try {
-          // 提现逻辑
-          console.log("Withdraw initiated");
-          const transactionBatch = values.transactionIdBytes32?.split(",")
-          const txWithdraw = await writeContract(config, {
-            address: echoooMallPaymentAddress,
-            abi,
-            functionName: "withdrawFundsBatch",
-            args: [transactionBatch, values.receivingAddress],
-          });
-
-          setTxHash(txWithdraw);
-          message.success("Withdraw successful!");
-        } catch (error) {
-          message.error("Transaction error!");
-          console.error(error);
-        }
-      } else if (isRefund) {
-        try {
-          // 退款逻辑
-          const transactionBatch = values.transactionIdBytes32?.split(",")
-          const txReceive = await writeContract(config, {
-            address: echoooMallPaymentAddress,
-            abi,
-            functionName: "refundOrdersBatch",
-            args: [transactionBatch],
-          });
-          setTxHash(txReceive);
-          message.success("Refund successful!");
-        } catch (error) {
-          message.error("Transaction error!");
-          console.error(error);
-        }
+      if (currentAction === "withdraw") {
+        const transactionBatch = values.transactionIdBytes32?.split(",");
+        const txWithdraw = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "withdrawFundsBatch",
+          args: [transactionBatch, values.receivingAddress],
+        });
+        setTxHash(txWithdraw);
+        message.success("Withdraw successful!");
+      } else if (currentAction === "refund") {
+        const transactionBatch = values.transactionIdBytes32?.split(",");
+        const txRefund = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "refundOrdersBatch",
+          args: [transactionBatch],
+        });
+        setTxHash(txRefund);
+        message.success("Refund successful!");
+      } else if (currentAction === "lockTx") {        
+        const txLock = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "lockTransaction",
+          args: [values.transactionIdBytes32],
+        });
+        setTxHash(txLock);
+        message.success("Lock Transaction successful!");
+      } else if (currentAction === "unlockTx") {
+        const txUnlock = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "unlockTransaction",
+          args: [values.transactionIdBytes32],
+        });
+        setTxHash(txUnlock);
+        message.success("Unlock Transaction successful!");
+      } else if (currentAction === "lockUser") {
+        const txLockUser = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "lockUser",
+          args: [values.address],
+        });
+        setTxHash(txLockUser);
+        message.success("Lock User successful!");
+      } else if (currentAction === "unlockUser") {
+        const txUnlockUser = await writeContract(config, {
+          address: echoooMallPaymentAddress,
+          abi,
+          functionName: "unlockUser",
+          args: [values.address],
+        });
+        setTxHash(txUnlockUser);
+        message.success("Unlock User successful!");
       } else {
         await writeContract(config, {
           address: USDCAddress,
@@ -107,17 +127,10 @@ export default function Index() {
     }
   };
 
-  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
-    errorInfo
-  ) => {
-    console.log("Failed:", errorInfo);
-  };
-
   useEffect(() => {
     form.resetFields(); // 清空表单
-    setTxIdBytes32("");
     setTxHash("");
-  }, [isWithdraw, isRefund]);
+  }, [currentAction]);
 
   return (
     <div className="flex flex-col w-screen h-screen bg-[#f7f9fb]">
@@ -129,76 +142,78 @@ export default function Index() {
           labelCol={{ span: 8 }}
           wrapperCol={{ span: 16 }}
           style={{ maxWidth: 1000, position: "relative" }}
-          initialValues={{
-            remember: true,
-            currency: "USDC",
-          }}
+          initialValues={{ currency: "USDC" }}
           onFinish={onFinish}
-          onFinishFailed={onFinishFailed}
           autoComplete="off"
         >
           {/* 切换模式按钮 */}
           <Radio.Group
-            onChange={(e) => {
-              const value = e.target.value;
-              setIsWithdraw(value === "withdraw");
-              setIsRefund(value === "refund");
-            }}
-            value={isWithdraw ? "withdraw" : isRefund ? "refund" : "order"}
-            style={{
-              position: "absolute",
-              top: -40,
-              left: 200,
-            }}
+            onChange={(e) => setCurrentAction(e.target.value)}
+            value={currentAction}
+            style={{ position: "absolute", top: -40, left: 200 }}
           >
             <Radio.Button value="order">Order</Radio.Button>
             <Radio.Button value="withdraw">Withdraw</Radio.Button>
             <Radio.Button value="refund">Refund</Radio.Button>
+            <Radio.Button value="lockTx">LockTx</Radio.Button>
+            <Radio.Button value="unlockTx">UnlockTx</Radio.Button>
+            <Radio.Button value="lockUser">LockUser</Radio.Button>
+            <Radio.Button value="unlockUser">UnlockUser</Radio.Button>
           </Radio.Group>
+
           {/* 动态表单 */}
-          {isWithdraw ? (
-            <>
-              <Form.Item<FieldType>
-                label="Transaction ID Bytes32"
-                name="transactionIdBytes32"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your transaction id bytes32!",
-                  },
-                ]}
-              >
-                <Input className="ml-16" placeholder="transaction id bytes32 01,transaction id bytes32 02"/>
-              </Form.Item>
-              <Form.Item<FieldType>
-                label="Receiving Address"
-                name="receivingAddress"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your receiving address!",
-                  },
-                ]}
-              >
-                <Input className="ml-16" />
-              </Form.Item>
-            </>
-          ) : isRefund ? (
-            <>
-              <Form.Item<FieldType>
-                label="Transaction ID Bytes32"
-                name="transactionIdBytes32"
-                rules={[
-                  {
-                    required: true,
-                    message: "Please input your transaction id bytes32!",
-                  },
-                ]}
-              >
-                <Input className="ml-16" placeholder="transaction id bytes32 01,transaction id bytes32 02"/>
-              </Form.Item>
-            </>
-          ) : (
+          {(currentAction === "withdraw" ||
+            currentAction === "refund" ||
+            currentAction === "lockTx" ||
+            currentAction === "unlockTx") && (
+            <Form.Item<FieldType>
+              label="Transaction ID Bytes32"
+              name="transactionIdBytes32"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the transaction id bytes32!",
+                },
+              ]}
+            >
+              <Input
+                className="ml-16"
+                placeholder="Transaction ID Bytes32 01,Transaction ID Bytes32 02"
+              />
+            </Form.Item>
+          )}
+
+          {(currentAction === "lockUser" || currentAction === "unlockUser") && (
+            <Form.Item<FieldType>
+              label="Address"
+              name="address"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input the address!",
+                },
+              ]}
+            >
+              <Input className="ml-16" placeholder="address" />
+            </Form.Item>
+          )}
+
+          {currentAction === "withdraw" && (
+            <Form.Item<FieldType>
+              label="Receiving Address"
+              name="receivingAddress"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your receiving address!",
+                },
+              ]}
+            >
+              <Input className="ml-16" />
+            </Form.Item>
+          )}
+
+          {currentAction === "order" && (
             <>
               <Form.Item<FieldType>
                 label="Currency"
@@ -210,7 +225,7 @@ export default function Index() {
                   },
                 ]}
               >
-                <Select placeholder="Select Currency" className="ml-16">
+                <Select className="ml-16">
                   <Option value="USDC">USDC</Option>
                 </Select>
               </Form.Item>
@@ -227,12 +242,12 @@ export default function Index() {
                 <Input className="ml-16" />
               </Form.Item>
               <Form.Item<FieldType>
-                label="Merchan Address"
+                label="Merchant Address"
                 name="merchanAddress"
                 rules={[
                   {
                     required: true,
-                    message: "Please input your merchant address!",
+                    message: "Please input the merchant address!",
                   },
                 ]}
               >
@@ -240,9 +255,10 @@ export default function Index() {
               </Form.Item>
             </>
           )}
+
           <div className="flex justify-center flex-col w-screen items-center">
             <span>tx: {txHash}</span>
-            {!isRefund && !isWithdraw ? (
+            {currentAction === "order" ? (
               <span>transactionIdBytes32: {txIdBytes32}</span>
             ) : null}
           </div>
@@ -254,7 +270,7 @@ export default function Index() {
             }}
           >
             <Button type="primary" htmlType="submit">
-              {isWithdraw ? "Withdraw" : isRefund ? "Refund" : "Submit"}
+              Submit ({currentAction})
             </Button>
           </Form.Item>
         </Form>
